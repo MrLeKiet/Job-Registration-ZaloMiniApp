855d1235870f587a6db00b6b9efb77b015060799
MrLeKiet <kietsvis@gmail.com>
2025-10-30 14:22:35 +0700

    Change layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -17,10 +17,10 @@ import JobsPage from "@/pages/Jobs";
 import LaborerPage from "@/pages/Laborer";
 import NewsDetailPage from "@/pages/NewDetail";
 import ProfilePage from "@/pages/Profile";
-import EnterprisePage from "@/pages/RecruitmentForeigners";
 import RegisterPage from "@/pages/Register";
-import RecruitmentForeignersJobDetailPage from "@/pages/RecruitmentForeignersJobDetail";
 import RecruitmentPostPage from "@/pages/RecruitmentPost";
+import RecruitmentForeignersPage from "@/pages/RecruitmentForeigners";
+import RecruitmentForeignersJobDetailPage from "@/pages/RecruitmentForeignersJobDetail";
 const Layout = () => {
   return (
     <App theme={getSystemInfo().zaloTheme as AppProps["theme"]}>
@@ -37,11 +37,11 @@ const Layout = () => {
               <Route path="/jobs/:id" element={<JobsDetailPage />} />
               <Route path="/laborer" element={<LaborerPage />} />
               <Route path="/laborer/:id" element={<LaborerDetailPage />} />
-              <Route path="/enterprise" element={<EnterprisePage />} />
-              <Route path="/recruitmentForeigners/:id" element={<RecruitmentForeignersJobDetailPage />} />
               <Route path="/detail/:id" element={<JobsDetailPage />} />
               <Route path="/register" element={<RegisterPage />} />
               <Route path="/RecruitmentPost" element={<RecruitmentPostPage />} />
+              <Route path="/recruitmentForeigners/:id" element={<RecruitmentForeignersJobDetailPage />} />
+              <Route path="/RecruitmentForeigners" element={<RecruitmentForeignersPage />} />
             </AnimationRoutes>
           </MainLayout>
         </ZMPRouter>
